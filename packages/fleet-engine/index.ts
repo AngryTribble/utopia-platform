bb043243790a4870184c2ac05b25040ec6fe2b00
Jerry Brown <118921428+AngryTribble@users.noreply.github.com>
2026-07-13 20:23:43 -0400

    Update index.ts

--- packages/fleet-engine/index.ts
+++ packages/fleet-engine/index.ts
@@ -11,11 +11,6 @@ function createId(prefix: string): string {
   return `${prefix}_${crypto.randomUUID()}`;
 }
 
-export function calculateFleetCost(
-  fleet: Fleet,
-  cardEngine: CardEngine
-  ): FleetCostBreakdown {
-
 export function createFleet(
   name = "New Fleet",
   pointLimit = 100
